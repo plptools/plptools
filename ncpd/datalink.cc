465016f5af0939069d2467425dc6b5a717703cc7
Jason Morley <hello@jbmorley.co.uk>
2026-03-16 10:30:51 -1000

    Update comment to match main

--- ncpd/datalink.cc
+++ ncpd/datalink.cc
@@ -182,7 +182,7 @@ static void *data_pump_thread(void *arg) {
                 }
             }
 
-            // Reset if we're unable to process data.
+            // Reset if we were unable to establish a stable link.
             if (!isLinkStable) {
                 dataLink->internalReset();
             }
