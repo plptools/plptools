a84fef3e1ea0e9c9732857c1f34822436d8bee78
Jason Morley <hello@jbmorley.co.uk>
2026-04-02 07:12:36 -1000

    Fix doxygen builds

--- lib/rfsv.h
+++ lib/rfsv.h
@@ -427,7 +427,7 @@ public:
     *
     * @param from Name of the file on the Psion to be copied.
     * @param to Name of the destination file on the local machine.
-    * @param context Callback context passed to the callback function @ref func.
+    * @param context Callback context passed to the callback function @p func.
     * @param func Pointer to a function which gets called on every read.
     *             This function can be used to show some progress etc. May be
     *             set to NULL, where no callback is performed. If the callback
